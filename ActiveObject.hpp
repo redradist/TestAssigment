c91d87302e666c068d746636794d483b2aa4d40d
redradist <redradist@gmail.com>
2018-11-13 01:45:14 +0200

    Added Thread Safe pattern for ActiveObject

---
+++ ActiveObject.hpp
@@ -0,0 +1,44 @@
+#ifndef AUTOSAR_COMPONENT_HPP
+#define AUTOSAR_COMPONENT_HPP
+
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+
+#include <boost/lockfree/queue.hpp>
+
+#include "RandomBlockGenerator.hpp"
+
+namespace Autosar {
+
+class ActiveObject {
+ public:
+  ActiveObject(const size_t _queueSize)
+    : queue{_queueSize} {
+  }
+  ~ActiveObject() {
+  }
+
+  void push(Autosar::RandomBlockInfo * _blockInfoPtr) {
+    queue.push(_blockInfoPtr);
+    queue_var_.notify_all();
+  }
+
+  void pop(Autosar::RandomBlockInfo *& _blockInfo) {
+    if (queue.empty() || !queue.pop(_blockInfo)) {
+      std::unique_lock<std::mutex> lock{mutex_};
+      queue_var_.wait(lock, [=] {
+        return !queue.empty();
+      });
+    }
+  }
+
+ private:
+  std::mutex mutex_;
+  std::condition_variable queue_var_;
+  boost::lockfree::queue<Autosar::RandomBlockInfo *> queue;
+};
+
+}
+
+#endif  // AUTOSAR_COMPONENT_HPP
